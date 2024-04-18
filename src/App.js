6ca71a0ad174ca045d5f7172636bdb5b159cbd22
sonawanekavita01 <147019571+sonawanekavita01@users.noreply.github.com>
2024-04-19 00:58:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is My <code>First </code> React Application.
         </p>
         <a
           className="App-link"
